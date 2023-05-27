8a07daca0265bdfaedda9459ccb8e9875477a901
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-27 18:38:10 +0300

    Create first_task.cpp
    task number 1

---
+++ first_task.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double a , s;
+	cout << "Write the side of square:";
+	cin >> a;
+	P = 4*a;
+	cout << "The perimeter of the square is equal" << P;
