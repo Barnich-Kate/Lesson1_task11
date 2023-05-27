d021d5ecf9c6b0e877a9f4d829655a6085e7abe8
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-27 18:40:29 +0300

    Create task_2.cpp
    add solution of the task 2

---
+++ task_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double a , s;
+	cout << "Write the side of square:";
+	cin >> a;
+	s = a*a;
+	cout << "The area of the square is equal " << s;
+
+}
